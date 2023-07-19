a3d693c01122cbc9479c3700b6c8dddbf5d98475
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-07-19 17:22:24 +0300

    created users_controller, and show view, updated User model and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, foreign_key: :creator_id
+
+  def created_events
+    events
+  end
 end
