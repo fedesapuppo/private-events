740dad11c3c57676ff598a852c6895823499fcec
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-07-23 17:26:27 +0300

    show error messages when rendering new after validation fails

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,18 +16,8 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-  # def create
-  #   @event = current_user.events.build(event_params)
-  #   if @event.save
-  #     redirect_to @event, notice: "Event was successfully created."
-  #   else
-  #     render :new
-  #   end
-  # end
-
   def create
-    @event = Event.new(event_params)
-
+    @event = current_user.events.build(event_params)
     if @event.save
       redirect_to @event, notice: "Event was successfully created."
     else
